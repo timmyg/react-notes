2f0ba6b5cb0c2a7f832d13b22f9f5c352bc83586
tim <timmyg13@gmail.com>
2020-11-05 08:53:45 -0500

    more debug deployed env vars

--- api/notes.js
+++ api/notes.js
@@ -6,6 +6,10 @@ export default async (req, res) => {
     airtable_base: airtableBase,
     airtable_api_key: airtableApiKey,
   } = process.env;
+  console.log(process.env.airtable_base);
+  console.log(process.env.airtable_api_key);
+  console.log(object);
+  console.log({ airtableBase, airtableApiKey });
   const base = new Airtable({ apiKey: airtableApiKey }).base(airtableBase);
   const notesTable = base("notes");
   switch (req.method) {
